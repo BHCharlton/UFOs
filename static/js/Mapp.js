5e184c16d5f64152da98c3e754d99f6e4b9d5107
BHCharlton <bhcharlton@netscape.net>
2022-03-05 14:20:46 -0600

    end module

--- static/js/Mapp.js
+++ static/js/Mapp.js
@@ -35,27 +35,29 @@ function buildTable(data) {
     });
 }
 
-// // Add a filter function to elliminate bloat data we won't need
-// function handleClick() {
-//     // Grab the datetime value from the filter
-//     let date = d3.select("#datetime").property("value");
-//     let filteredData = tableData; 
+// Add a filter function to elliminate bloat data we won't need
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData; 
+    console.log(date)
 
-//     // Check to see if a date was entered and filter the data using that date
-//     if (date) {
-//         // Apply `filter` to the table data to only keep the
-//         // rows where the `datetime` value matches the filter value
-//         filteredData = filteredData.filter(row => row.datetime === date);
-//     };
-//     // Rebuild the table using the filtered data
-//     // @NOTE: If no date was entered, then filteredData will
-//     // just be the original tableData.
-//     buildTable(filteredData);
+    // Check to see if a date was entered and filter the data using that date
+    if (date) {
+        // Apply `filter` to the table data to only keep the
+        // rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+        console.log(filteredData)
+    };
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
     
-// };
+};
 
-// // Attach an event to listen for the form button
-// d3.selectAll("#filter-btn").on("click", handleClick);
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
 
-// // Build the table when the page loads
+// Build the table when the page loads
 buildTable(tableData);
